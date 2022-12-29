e8584cc367577733b4f553b729036ce0fa934a1f
Soraya Sanchez <sorayasanchez0503@gmail.com>
2022-12-29 15:39:28 -0500

    Tablero de productos y usuarios

---
+++ src/components/ContentRowMovies.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import SmallCard from './SmallCard';
+import { getProducts, getUsers } from '../services/api';
+
+// /*  Cada set de datos es un objeto literal */
+
+// /* <!-- Movies in DB --> */
+
+
+// let moviesInDB = {
+//     title: 'Movies in Data Base',
+//     color: 'primary', 
+//     cuantity: 21,
+//     icon: 'fa-clipboard-list'
+// }
+
+// /* <!-- Total awards --> */
+
+// let totalAwards = {
+//     title:' Total awards', 
+//     color:'success', 
+//     cuantity: '79',
+//     icon:'fa-award'
+// }
+
+// /* <!-- Actors quantity --> */
+
+// let actorsQuantity = {
+//     title:'Actors quantity' ,
+//     color:'warning',
+//     cuantity:'49',
+//     icon:'fa-user-check'
+// }
+
+
+// let cartPropss = [moviesInDB, totalAwards, actorsQuantity];
+
+function ContentRowMovies(){
+    const [cartProps, setCartProps] = useState([])
+     
+     useEffect(()=>{
+         fetchData()
+        },[])
+
+      const fetchData = async()=>{
+        const promises = [getProducts(), getUsers()] // lista de promesas para obtener informacion de products, usuarios
+        const response = await Promise.allSettled(promises) // ejecutar todas promesas a la vez
+        setCartProps([
+          {
+            title: 'Productos',
+            color: 'primary',
+            cuantity: response[0].value.count.toString(),
+            icon:'fa-user-check',
+          },
+          {
+            title: 'Users',
+            color: 'primary',
+            cuantity: response[1].value.count.toString(),
+            icon:'fa-user-check',
+          }
+        ])
+       }
+
+    return (
+    
+        <div className="row">
+            
+            {cartProps.map( (movie, i) => {
+
+                return <SmallCard {...movie} key={i}/>
+            
+            })}
+
+        </div>
+    )
+}
+
+export default ContentRowMovies;
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+const pathApi = 'http://localhost:3030/api'
+
+export const getProducts = async()=>{
+  const products = await axios.get(`${pathApi}/products`)
+  return products.data
+}
+export const getUsers = async()=>{
+  const users = await axios.get(`${pathApi}/users`)
+  return users.data
+}
+
